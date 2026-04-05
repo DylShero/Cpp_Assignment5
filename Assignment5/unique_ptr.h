21acef33187de11a71cdc9719597be70931573de
DylShero <dsdylansheridan17@gmail.com>
2026-04-05 17:58:49 +0100

    Makefile and unique_ptr implementation

--- Assignment5/unique_ptr.h
+++ Assignment5/unique_ptr.h
@@ -16,6 +16,16 @@ namespace HPC
 		/// TODO: Take care of declarations for move and copy semantics
 		/// 	  Write function definitions outside class body
 
+		//Deleted copy constructor
+		unique_ptr(const unique_ptr& in) = delete;
+		//Deletec copy assignment operator
+    	unique_ptr& operator=(const unique_ptr& rhs) = delete;
+
+		//Move constructor
+		unique_ptr(unique_ptr&& in);
+		//Move assignment operator
+    	unique_ptr& operator=(unique_ptr&& rhs);
+
 		// Destructor
 		~unique_ptr ();
 		
@@ -49,9 +59,65 @@ namespace HPC
 		std::println(" containing null pointer");
 	    }
 	    else{
-		std::println(" with value ", *managed_object);
+		std::println(" with value {}", *managed_object);
 	    }
 	    delete managed_object;
+	}
+
+	//Constructor
+	template <typename T>
+	unique_ptr<T>::unique_ptr(T* in): managed_object{in}{
+		std::print("Constructing HPC::unique_ptr for type {}", typeid(managed_object).name());
+		if(managed_object==nullptr){
+		std::println(" containing null pointer");
+	    }
+	    else{
+		std::println(" with value {}", *managed_object);
+	    }
+	}
+	
+
+	//Move constructor
+	template <typename T>
+	unique_ptr<T>::unique_ptr(unique_ptr&& in){
+		std::println("Calling HPC::unique_ptr move constructor");
+		this->managed_object = in.managed_object;
+		in.managed_object = nullptr;
+	}
+
+	//Move assignment
+	template <typename T>
+	unique_ptr<T>& unique_ptr<T>::operator=(unique_ptr<T>&& rhs){
+		if(this->managed_object == rhs.managed_object){
+			std::println("Called on same object");
+			return *this;
+		}
+		std::println("Calling HPC::unique_ptr move assignment operator");
+		delete this->managed_object;
+		this->managed_object = rhs.managed_object;
+		rhs.managed_object = nullptr;
+		return *this;
+	}
+
+	template <typename T>
+	T& unique_ptr<T>::operator*(){
+		return *(this->managed_object);
+	}
+
+	template <typename T>
+	T unique_ptr<T>::operator*() const{
+		return this->managed_object;
+	}
+
+	template <typename T>
+	T* unique_ptr<T>::get(){
+		return managed_object;
+	}
+
+	template <typename T>
+	T* unique_ptr<T>::release(){
+		this->managed_object = nullptr;
+		return this->managed_object;
 	}
 
 } /* HPC */ 
