3d6ba444193a357216f02da492cc9a86902fc4df
DylShero <dsdylansheridan17@gmail.com>
2026-04-06 12:14:09 +0100

    Added 5d functor implementation

--- Assignment5/assignment5d.cc
+++ Assignment5/assignment5d.cc
@@ -7,6 +7,19 @@
 /// Write functor is_lower_than  
 /// to use instead of lambda below in the remove_if algorithm
 
+class is_lower_than
+{
+private:
+	double cutoff_value;
+public:
+	is_lower_than(double cv) : cutoff_value{cv} {}
+
+	int operator () (int in) const {
+		if(in < cutoff_value) return true;
+		else return false;
+	}
+};
+
 int main(void)
 {
 	std::vector V {-1.2, 5.4, 12.2, -3.9, -0.4, 13.1, 0.5}; 
@@ -17,7 +30,7 @@ int main(void)
 		return testval <  cutoff_value;
 	};
 
-	auto new_it = std::remove_if(std::begin(V), std::end(V), lambda);
+	auto new_it = std::remove_if(std::begin(V), std::end(V), is_lower_than(cutoff_value));
 	V.erase(new_it, std::end(V));
 
 	// print filtered vector to screen
